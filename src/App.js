e1c55da2d134517ae2391778f42c784e004e8b48
Nitheesh6303 <patnamgurunitheeshkumar@gmail.com>
2024-04-05 11:27:56 +0530

    Qwack Solution

---
+++ src/App.js
@@ -0,0 +1,40 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="navbar">
+    <div className="subNav">
+    <a href="#image"><img src="/static/media/logo.2a487917892639ad5b74.png" alt="logo"></img></a>
+  <a href="#home">Home</a>
+  <a href="#about">About</a>
+  <a href="#services">Services</a>
+  <a href="#contact">Worksheets</a>
+    </div>
+    <div>
+    <a href="button"><button>Login</button></a>
+    </div>
+
+    <div className="waveWrapper waveAnimation">
+      
+  <div className="waveWrapperInner bgTop">
+    <div className="wave waveTop">
+    
+
+      
+    </div>
+  </div>
+  <div className="waveWrapperInner bgMiddle">
+    <div className="wave waveMiddle">
+    </div>
+  </div>
+  <div className="waveWrapperInner bgBottom">
+    <div className="wave waveBottom"></div>
+  </div>
+</div>
+</div>
+
+  );
+}
+
+export default App;
